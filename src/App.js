4fc4456695e5f27500b868667280e8936e74dd20
TUANTRAN <dictuantran@gmail.com>
2020-06-16 11:38:05 +0700

    add react-admin component

--- src/App.js
+++ src/App.js
@@ -1,64 +1,13 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Admin, Resource } from 'react-admin';
 import './App.css';
-//import ItemService from './shared/mock-item-service.js';
-import ItemService from 'itemService.js';
-import { render } from 'react-dom';
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-        this.itemService = new ItemService();
-        
-        this.state = {
-            showDetails: false,
-            editItem: false,
-            selectedItem: null,
-            newItem: null
-        }
-    }
-
-    componentDidMount(){
-        this.getItems();
-        this.getTaxCodes();
-    }
-
-    render() {
-        const items = this.state.items;
-        if(!items) return null;
-        const showDetails = this.state.showDetails;
-        const selectedItem = this.state.selectedItem;
-        const newItem = this.state.newItem;
-        const editItem = this.state.editItem;
-        const listItems = items.map((item) => 
-            <li key={item.link}> 
-                <span className="item-name">{item.name}</span>&nbsp;|&nbsp; {item.summary}
-            </li>
-        )
-
-        return (
-            <div className="App">
-                <ul className="items">
-                    {listItems}
-                </ul>
-                <br />
-                <button type="button" name="button"></button>
-                <br />
-            </div>
-        );
-    }
-
-    getItems(){
-        this.itemService.retrieveItems().then(items => {
-            this.setState({items: items});
-        })
-    }
-
-    getTaxCodes(){
-        console.log("getTaxCodes");
-        this.itemService.retrieveTaxcode().then(taxcodes => {
-            console.log(taxcodes);
-        })
-    }
+const App = () => {
+    return (
+        <Admin>
+            <Resource name="Orders"></Resource>
+        </Admin>
+    );
 }
 
 export default App;
